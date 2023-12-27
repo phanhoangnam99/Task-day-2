9c5777b24ab3fd0f7db2c542ef77c045ab85f30a
nam <johndoe@example.com>
2023-12-27 17:24:22 +0700

    first commit

---
+++ .eslintrc.cjs
@@ -0,0 +1,32 @@
+module.exports = {
+  root: true,
+  env: { browser: true, es2020: true },
+  extends: [
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:react/jsx-runtime",
+    "plugin:react-hooks/recommended",
+  ],
+  ignorePatterns: ["dist", ".eslintrc.cjs"],
+  parserOptions: { ecmaVersion: "latest", sourceType: "module" },
+  settings: {
+    react: {
+      // Nói eslint-plugin-react tự động biết version của React.
+      version: "detect",
+    },
+    // Nói ESLint cách xử lý các import
+    "import/resolver": {
+      node: {
+        paths: [path.resolve(__dirname, "")],
+        extensions: [".js", ".jsx", ".ts", ".tsx"],
+      },
+    },
+  },
+  plugins: ["react-refresh"],
+  rules: {
+    "react-refresh/only-export-components": [
+      "warn",
+      { allowConstantExport: true },
+    ],
+  },
+};
