3ec61b001c27a8c7f2f5967b712ad5d3eb9cfe7a
nam <johndoe@example.com>
2023-12-27 17:36:53 +0700

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import "./App.css";
 import { useDispatch, useSelector } from "react-redux";
 import { increment } from "./features/counter/counterSlice";
 import { Button } from "antd";
+import Form from "./components/Form";
 
 function App() {
   const count = useSelector((state) => state.counter.value);
@@ -12,18 +13,45 @@ function App() {
 
   return (
     <>
-     <Button >Button</Button>
+      <Button>Test Antd</Button>
+      <Form />
+
       <div>
-        <a href="https://vitejs.dev" target="_blank">
+        <a
+          href="https://vitejs.dev"
+          target="_blank"
+          className="flex justify-center"
+        >
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
-        <a href="https://react.dev" target="_blank">
+        <a
+          href="https://react.dev"
+          target="_blank"
+          className="flex justify-center"
+        >
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={()=>dispatch(increment())}>count is {count}</button>
+        <p>Click here to test Redux</p>
+        <div className='flex justify-center py-2'>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke-width="1.5"
+            stroke="currentColor"
+            class="w-6 h-6"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="M19.5 13.5 12 21m0 0-7.5-7.5M12 21V3"
+            />
+          </svg>
+        </div>
+        <button onClick={() => dispatch(increment())}>count is {count}</button>
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
---
+++ src/hooks/ExampleHooks.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function ExampleHooks() {
+  return (
+    <div>ExampleHooks</div>
+  )
+}
